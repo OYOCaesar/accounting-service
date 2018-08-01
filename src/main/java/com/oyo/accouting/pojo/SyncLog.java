e9e4971fc0c2a39eb10e797e5b6d61d5a208238c
zfeng321 <zephyr.guo@oyohotels.cn>
2018-08-01 16:42:04 +0800

    添加Hotel，syncLog的基础操作代码

--- src/main/java/com/oyo/accouting/pojo/SyncLog.java
+++ src/main/java/com/oyo/accouting/pojo/SyncLog.java
@@ -1,8 +1,13 @@
 package com.oyo.accouting.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Getter
+@Setter
 @Table(name = "sync_log")
 public class SyncLog {
 
@@ -15,57 +20,5 @@ public class SyncLog {
   private java.sql.Timestamp createTime;
 
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-
-  public Long getSourceId() {
-    return sourceId;
-  }
-
-  public void setSourceId(Long sourceId) {
-    this.sourceId = sourceId;
-  }
-
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-
-  public Long getVersion() {
-    return version;
-  }
-
-  public void setVersion(Long version) {
-    this.version = version;
-  }
-
-
-  public java.sql.Timestamp getUpdateTime() {
-    return updateTime;
-  }
-
-  public void setUpdateTime(java.sql.Timestamp updateTime) {
-    this.updateTime = updateTime;
-  }
-
-
-  public java.sql.Timestamp getCreateTime() {
-    return createTime;
-  }
-
-  public void setCreateTime(java.sql.Timestamp createTime) {
-    this.createTime = createTime;
-  }
 
 }
