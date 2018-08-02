1c294a67116cc616d3edef31107daafd489e787c
zfeng321 <zephyr.guo@oyohotels.cn>
2018-08-02 17:22:55 +0800

    添加Hotel，syncLog的基础操作代码

---
+++ src/main/java/com/oyo/accouting/bean/PlanTemplet.java
@@ -0,0 +1,29 @@
+package com.oyo.accouting.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlanTemplet {
+
+    private String status;
+
+    private String uniqueCode;
+
+    private String oyoId;
+
+    private String hotelId;
+
+    private String hotelName;
+
+    private String city;
+
+    private String zoneName;
+
+    private String contractType;
+
+    private String fixedRate;
+
+    private String OyoShare;
+}
--- src/main/java/com/oyo/accouting/util/ApnExcelParseTool.java
+++ src/main/java/com/oyo/accouting/util/ApnExcelParseTool.java
@@ -1,5 +1,7 @@
 package com.oyo.accouting.util;
 
+import com.oyo.accouting.bean.PlanTemplet;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -9,9 +11,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author zfguo
@@ -43,9 +48,9 @@ public class ApnExcelParseTool {
         return workbook;
     }
 
-    public static void parseWorkbook(Workbook workbook, List<Object> apnModelList) {
+    public static void parseWorkbook(Workbook workbook, List<PlanTemplet> apnModelList) {
         int numOfSheet = workbook.getNumberOfSheets();
-
+        numOfSheet = 1;
         //依次解析每一个Sheet
         for (int i = 0; i < numOfSheet; ++i) {
             Sheet sheet = workbook.getSheetAt(i);
@@ -56,26 +61,23 @@ public class ApnExcelParseTool {
     //保存需要调用的ApnModel中的方法
     private static List<Method> mUsedMethod;
 
-    private static void parseSheet(Sheet sheet, List<Object> apnModelList) {
+    private static void parseSheet(Sheet sheet, List<PlanTemplet> apnModelList) {
         Row row;
-
-        int count = 0;
 
         //利用迭代器，取出每一个Row
         Iterator<Row> iterator = sheet.iterator();
         while(iterator.hasNext()) {
             row = iterator.next();
-
+            int rowNum = row.getRowNum();
             //由于第一行是标题，因此这里单独处理
-            if (count == 0) {
-                mUsedMethod = new ArrayList<>();
+            if (rowNum == 0) {
+                mUsedMethod = new LinkedList<>();
                 parseRowAndFindMethod(row);
             } else {
                 //其它行都在这里处理
                 parseRowAndFillData(row, apnModelList);
             }
 
-            ++count;
         }
     }
 
@@ -88,10 +90,10 @@ public class ApnExcelParseTool {
         try {
             //根据String得到需要调用的ApnModel中的方法
             for (String str : rst) {
-                methodName = "set" + str;
+                methodName = "set" + regexMethodName(str);
                 //反射拿到method
                 mUsedMethod.add(
-                        Object.class.getDeclaredMethod(methodName, String.class));
+                        PlanTemplet.class.getDeclaredMethod(methodName, String.class));
             }
         } catch (NoSuchMethodException e) {
             System.out.println(e.toString());
@@ -100,13 +102,43 @@ public class ApnExcelParseTool {
     }
 
     /**
+     * 处理excel中不规范的方法名称
+     * @param str
+     * @return
+     */
+   private static String regexMethodName(String str){
+
+       try {
+           Pattern p = Pattern.compile("[^a-zA-Z_ ]");
+           Matcher m = p.matcher(str);
+           boolean hasInvalid = m.find();
+           int i = hasInvalid ? m.start() : str.length(); //过滤掉无用的字符
+           //第一个字母处理成大写，其他全部处理为小写
+           str = str.substring(0, 1).toUpperCase() + str.substring(1, i).trim().toLowerCase();
+           Pattern p1 = Pattern.compile("[_ ]{1}[a-zA-Z]{1}");
+           Matcher m1 = p1.matcher(str);
+           while (m1.find()) {
+               int i1 = m1.start();
+               //匹配空格+字母和_+字母，并替换为空格或_后面那个字母的大写
+               String i1Str = str.substring(i1 + 1, i1 + 2).toUpperCase();
+               str = m1.replaceFirst(i1Str);
+               m1 = p1.matcher(str);
+           }
+       }catch (Exception e){
+           e.printStackTrace();
+       }
+       return str;
+    }
+
+
+    /**
      * 开始解析具体的数据
      */
-    private static void parseRowAndFillData(Row row, List<Object> apnModelList) {
+    private static void parseRowAndFillData(Row row, List<PlanTemplet> apnModelList) {
         //同样利用parseRow得到具体每一行的数据
         List<String> rst = parseRow(row);
 
-        Object apnModel = new Object();
+        PlanTemplet apnModel = new PlanTemplet();
 
         //这里主要debug一下，避免由于Excel的格式可能不太对
         //使得每一行的数据解析地不太对
@@ -131,7 +163,7 @@ public class ApnExcelParseTool {
      * 这里是解析每一行的代码
      */
     private static List<String> parseRow(Row row) {
-        List<String> rst = new ArrayList<>();
+        List<String> rst = new LinkedList<>();
 
         Cell cell;
 
@@ -140,14 +172,40 @@ public class ApnExcelParseTool {
         while (iterator.hasNext()) {
             cell = iterator.next();
 
-            //定义每一个cell的数据类型
-            cell.setCellType(CellType.STRING);
-
             //取出cell中的value
-            rst.add(cell.getStringCellValue());
+            String value = null;
+            switch (cell.getCellTypeEnum()){
+                case NUMERIC:
+                    if (HSSFDateUtil.isCellDateFormatted(cell)) {
+                        //注：format格式 yyyy-MM-dd hh:mm:ss 中小时为12小时制，若要24小时制，则把小h变为H即可，yyyy-MM-dd HH:mm:ss
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        value=sdf.format(HSSFDateUtil.getJavaDate(cell.
+                                getNumericCellValue())).toString();
+                    } else {
+                        value = String.valueOf(cell.getNumericCellValue());
+                    }
+                    break;
+                case STRING:
+                    value = cell.getStringCellValue();
+                    break;
+            }
+            rst.add(value);
         }
-
         return rst;
+    }
+
+    public static void main(String[] args){
+        setFilePath("/Users/oyo/Desktop/aa.xlsx");
+        try {
+            Workbook workbook = initWorkBook();
+            List<PlanTemplet> apnModelList = new LinkedList<>();
+            parseWorkbook(workbook,apnModelList);
+            for(PlanTemplet p : apnModelList){
+                System.out.println(p.getOyoId()+"=="+p.getHotelId()+"=="+p.getOyoShare());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
