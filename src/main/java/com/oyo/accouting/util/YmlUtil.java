114ac2ad718bd443144cb8e4d261a5e6dcc615eb
zhangsuyun2018 <suyun.zhang@oyohotels.cn>
2018-08-08 19:14:24 +0800

    commit

---
+++ src/main/java/com/oyo/accouting/util/YmlUtil.java
@@ -0,0 +1,26 @@
+package com.oyo.accouting.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 读取yml文件中的值
+ * @author EDZ
+ *
+ */
+@Configuration
+public class YmlUtil {
+	
+	// 获取sapWebServiceUrl属性
+	public static String sapWebServiceUrl;
+
+	public static String getSapWebServiceUrl() {
+		return sapWebServiceUrl;
+	}
+
+	@Value("${sapWebServiceUrl}")
+	public void setSapWebServiceUrl(String sapWebServiceUrl) {
+		YmlUtil.sapWebServiceUrl = sapWebServiceUrl;
+	}
+	
+}
