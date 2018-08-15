4dc53a63459f85292ddb33e4d7571bdac3742672
zhangsuyun2018 <suyun.zhang@oyohotels.cn>
2018-08-15 17:44:36 +0800

    commit;

--- src/main/java/com/oyo/accouting/job/SyncHotelJob.java
+++ src/main/java/com/oyo/accouting/job/SyncHotelJob.java
@@ -2,8 +2,11 @@ package com.oyo.accouting.job;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import com.oyo.accouting.service.SyncHotelToSapService;
 
 public class SyncHotelJob implements BaseJob {
 	private static Logger _log = LoggerFactory.getLogger(SyncHotelJob.class);
@@ -14,8 +17,16 @@ public class SyncHotelJob implements BaseJob {
 	}
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-
-		_log.info("aaaaa");
+		ApplicationContext applicationContext;
+		try {
+			applicationContext = (ApplicationContext) context.getScheduler().getContext().get("applicationContext");
+			SyncHotelToSapService service = applicationContext.getBean(SyncHotelToSapService.class);
+			String result = service.syncHotelToSap();
+			_log.info(result);
+		} catch (SchedulerException e) {
+			_log.error("Sync Ar and Ap and Journal Entry data to sap throwing exception!");
+			e.printStackTrace();
+		}
 	}
 
 }
