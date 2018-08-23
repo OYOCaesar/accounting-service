99f3f1467d63d38dd771e4648cd51c165fc6f193
zhangsuyun2018 <suyun.zhang@oyohotels.cn>
2018-08-23 15:30:46 +0800

    commit;

--- src/main/java/com/oyo/accouting/controller/QueryCrsAccountPeriodController.java
+++ src/main/java/com/oyo/accouting/controller/QueryCrsAccountPeriodController.java
@@ -98,5 +98,77 @@ public class QueryCrsAccountPeriodController {
 			log.error("Export Merchant Account throwing exception:{}", e);
 		}
 	}
+	
+	//汇总统计导出
+	@RequestMapping("exportSummaryStatistics")
+	public void exportSummaryStatistics(HttpServletResponse response, QueryAccountPeriodDto queryAccountPeriodDto) {
+		try {
+			// 此处为标题，excel首行的title，按照此格式即可，格式无需改动，但是可以增加或者减少项目。
+			String export = "OYO订单编号#orderNo,顾客名字#guestName,预定人名称#bookingGuestName,预定人名称2#bookingSecondaryGuestName,订单来源#orderChannel,"
+					      + "OYO酒店编号#oyoId,OYO酒店名称#hotelName,营业收入#orderTotalAmount,入住时间#checkInDate,离店时间#checkOutDate,间/夜#currentMonthRoomsNumber,"
+					      + "费率#currentMonthRate,顾客选择方式#paymentType,平台名称#otaName,平台订单号#otaId,城市#city,区域#region,营收核对结果#revenueCheckResults,"
+					      + "营收差异原因#reasonsForRevenueDifference,提成比例#proportions,顾客选择方式核对结果#paymentTypeCheckingResult,平台费承担方#platformFeePayableParty,"
+					      + "备注#remarks";
+			String[] excelHeader = export.split(",");
+			
+			LocalDate localDate = LocalDate.now();
+    		//如果开始结束账期都为空，那么开始结束账期均为当前月所在的账期
+    		if (StringUtils.isEmpty(queryAccountPeriodDto.getStartYearAndMonthQuery()) && StringUtils.isEmpty(queryAccountPeriodDto.getEndYearAndMonthQuery())) {
+    			queryAccountPeriodDto.setStartYearAndMonthQuery(localDate.getYear() + "-" + (localDate.getMonthValue() < 10 ? "0" + localDate.getMonthValue() : localDate.getMonthValue()));
+    			queryAccountPeriodDto.setEndYearAndMonthQuery(localDate.getYear() + "-" + (localDate.getMonthValue() < 10 ? "0" + localDate.getMonthValue() : localDate.getMonthValue()));
+    		} else {
+    			if (StringUtils.isNotEmpty(queryAccountPeriodDto.getStartYearAndMonthQuery()) && StringUtils.isEmpty(queryAccountPeriodDto.getEndYearAndMonthQuery())) {
+        			queryAccountPeriodDto.setEndYearAndMonthQuery(queryAccountPeriodDto.getStartYearAndMonthQuery());
+        		}
+        		if (StringUtils.isEmpty(queryAccountPeriodDto.getStartYearAndMonthQuery()) && StringUtils.isNotEmpty(queryAccountPeriodDto.getEndYearAndMonthQuery())) {
+        			queryAccountPeriodDto.setStartYearAndMonthQuery(queryAccountPeriodDto.getEndYearAndMonthQuery());
+        		}
+    		}
+    		List<AccountPeriodDto> list = queryCrsAccountPeriodService.queryCrsAccountPeriod(queryAccountPeriodDto);
+    		Map<Integer,List<AccountPeriodDto>> hotelGroupMap = list.stream().collect(Collectors.groupingBy(AccountPeriodDto::getUniqueCode));
+    		for (Map.Entry<Integer, List<AccountPeriodDto>> entry : hotelGroupMap.entrySet()) {
+				String className = entry.getKey() + "-" + queryAccountPeriodDto.getStartYearAndMonthQuery().replace("-", "") + "-商户对账单"; // className:生成的excel默认文件名和sheet页
+				ExcelUtils.export(response, className, excelHeader, entry.getValue());// 调用封装好的导出方法
+		    }
+		} catch (Exception e) {
+			log.error("Export Merchant Account throwing exception:{}", e);
+		}
+	}
+	
+	//明细导出
+	@RequestMapping("exportDetails")
+	public void exportDetails(HttpServletResponse response, QueryAccountPeriodDto queryAccountPeriodDto) {
+		try {
+			// 此处为标题，excel首行的title，按照此格式即可，格式无需改动，但是可以增加或者减少项目。
+			String export = "OYO订单编号#orderNo,顾客名字#guestName,预定人名称#bookingGuestName,预定人名称2#bookingSecondaryGuestName,订单来源#orderChannel,"
+					      + "OYO酒店编号#oyoId,OYO酒店名称#hotelName,营业收入#orderTotalAmount,入住时间#checkInDate,离店时间#checkOutDate,间/夜#currentMonthRoomsNumber,"
+					      + "费率#currentMonthRate,顾客选择方式#paymentType,平台名称#otaName,平台订单号#otaId,城市#city,区域#region,营收核对结果#revenueCheckResults,"
+					      + "营收差异原因#reasonsForRevenueDifference,提成比例#proportions,顾客选择方式核对结果#paymentTypeCheckingResult,平台费承担方#platformFeePayableParty,"
+					      + "备注#remarks";
+			String[] excelHeader = export.split(",");
+			
+			LocalDate localDate = LocalDate.now();
+    		//如果开始结束账期都为空，那么开始结束账期均为当前月所在的账期
+    		if (StringUtils.isEmpty(queryAccountPeriodDto.getStartYearAndMonthQuery()) && StringUtils.isEmpty(queryAccountPeriodDto.getEndYearAndMonthQuery())) {
+    			queryAccountPeriodDto.setStartYearAndMonthQuery(localDate.getYear() + "-" + (localDate.getMonthValue() < 10 ? "0" + localDate.getMonthValue() : localDate.getMonthValue()));
+    			queryAccountPeriodDto.setEndYearAndMonthQuery(localDate.getYear() + "-" + (localDate.getMonthValue() < 10 ? "0" + localDate.getMonthValue() : localDate.getMonthValue()));
+    		} else {
+    			if (StringUtils.isNotEmpty(queryAccountPeriodDto.getStartYearAndMonthQuery()) && StringUtils.isEmpty(queryAccountPeriodDto.getEndYearAndMonthQuery())) {
+        			queryAccountPeriodDto.setEndYearAndMonthQuery(queryAccountPeriodDto.getStartYearAndMonthQuery());
+        		}
+        		if (StringUtils.isEmpty(queryAccountPeriodDto.getStartYearAndMonthQuery()) && StringUtils.isNotEmpty(queryAccountPeriodDto.getEndYearAndMonthQuery())) {
+        			queryAccountPeriodDto.setStartYearAndMonthQuery(queryAccountPeriodDto.getEndYearAndMonthQuery());
+        		}
+    		}
+    		List<AccountPeriodDto> list = queryCrsAccountPeriodService.queryCrsAccountPeriod(queryAccountPeriodDto);
+    		Map<Integer,List<AccountPeriodDto>> hotelGroupMap = list.stream().collect(Collectors.groupingBy(AccountPeriodDto::getUniqueCode));
+    		for (Map.Entry<Integer, List<AccountPeriodDto>> entry : hotelGroupMap.entrySet()) {
+				String className = entry.getKey() + "-" + queryAccountPeriodDto.getStartYearAndMonthQuery().replace("-", "") + "-商户对账单"; // className:生成的excel默认文件名和sheet页
+				ExcelUtils.export(response, className, excelHeader, entry.getValue());// 调用封装好的导出方法
+		    }
+		} catch (Exception e) {
+			log.error("Export Merchant Account throwing exception:{}", e);
+		}
+	}
 
 }
